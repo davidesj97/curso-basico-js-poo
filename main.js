79e2c98ef68b97e8f9cb6e77f7b0e300dd91f203
David Sánchez <davidesj97@gmail.com>
2022-10-24 14:11:20 -0500

    clases, metodos e instancias

--- main.js
+++ main.js
@@ -1,41 +1,67 @@
-const natalia = {
-  name: "Natalia",
-  age: 20,
-  cursosAprobados: [
-    "Curso Definitico de HTML y CSS",
-    "Curso Práctico de HTML y CSS"
-  ],
-  aprobarCurso(nuevoCurso) {
-    this.cursosAprobados.push(nuevoCurso);
-  },
-}
-
-
-function Student (name, age, cursosAprobados) {
-  this.name = name;
-  this.age = age;
-  this.cursosAprobados = cursosAprobados;
-}
-
-Student.prototype.aprobarCurso = function (nuevoCurso) {
-  this.cursosAprobados.push(nuevoCurso);
-}
-
-const Juanita = new Student("Juanita", 25, [])
-
-// Prototipos con la sintaxis de clases
-
-class Student2 {
-  constructor(name, age, cursosAprobados) {
+class Student {
+  constructor({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = []
+  }) {
     this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-  }
-
-  aprobarCurso(nuevoCurso) {
-    this.cursosAprobados.push(nuevoCurso);
-    return nuevoCurso;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+      twitter,
+      instagram,
+      facebook
+    };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
   }
 }
 
-const Pablo = new Student2("Pablo Ramirez", 18, []);
+class LearningPaths {
+  constructor({
+    name,
+    courses
+  }) {
+    this.name = name;
+    this.courses = courses;
+  }
+}
+
+const JavaScript = new LearningPaths({
+  name: "JavaScript a Profundidad",
+  courses: [
+    "Curso Básico de JavaScript",
+    "Curso Práctico de JavaScript",
+    "Taller Práctico de JavaScript: Matemáticas y Estadística Básica",
+    "Taller Práctico de JavaScript: ¡Crea tu Primer Videojuego!",
+    "Curso de Closures y Scope en JavaScript",
+    "Curso de ECMAScript: Historia y Versiones de JavaScript",
+  ]
+})
+
+const GameDeveloper = new LearningPaths({
+  name: "Game Developer",
+  courses: [
+    "Curso de Computación Básica",
+    "Nuevo Curso Gratis de Programación Básica",
+    "Curso Profesional de Git y GitHub",
+    "Curso de Introducción a C#",
+    "Curso de C++ Básico",
+    "Curso de C# para Videojuegos",
+  ]
+})
+
+const david = new Student({
+  name: "David",
+  username: "davidesj97",
+  email: "david@platzi.com",
+  learningPaths: [
+    JavaScript,
+    GameDeveloper
+  ]
+})
